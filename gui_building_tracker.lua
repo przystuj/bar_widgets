0fbe50886c61597ce8ce15a561c5a2c6ed97e878
SuperKitowiec <superkitowiec@gmail.com>
2024-05-27 22:33:46 +0200

    Cache Stacks to avoid recreating them in each frame

--- gui_building_tracker.lua
+++ gui_building_tracker.lua
@@ -1,9 +1,11 @@
+local widgetName = "Buildings Tracker"
+
 function widget:GetInfo()
     return {
-        name = "Buildings Tracker",
+        name = widgetName,
         desc = "Shows counters for pinpointers, nukes and junos. Click building icon to select one, shift click to select all",
         author = "SuperKitowiec",
-        version = 0.3,
+        version = 0.4,
         license = "GNU GPL, v2 or later",
         layer = 0
     }
@@ -15,28 +17,24 @@ end
 
 local MasterFramework
 local requiredFrameworkVersion = 42
-local key
-local backgroundColor
-local contentStack
-local red
-local green
-local yellow
-local white
+local key, contentStack, countersCache
+local red, green, yellow, white, backgroundColor
 local spectatorMode
+local pinpointersId, nukesId, junosId = "pinpointers", "nukes", "junos"
 
 local nukeNames = {
-	armsilo = true, 		-- arm nuke
-	corsilo = true, 		-- cor nuke
+    armsilo = true,
+    corsilo = true,
 }
 
 local junoNames = {
-	armjuno = true, 		-- arm juno
-	corjuno = true 		    -- cor juno
+    armjuno = true,
+    corjuno = true,
 }
 
 local pinpointerNames = {
-	armtarg = true, 		-- arm pinpointer
-	cortarg = true, 		-- cor pinpointer
+    armtarg = true,
+    cortarg = true,
 }
 
 local junoDefIDs = {}
@@ -54,17 +52,13 @@ for unitDefID, unitDef in pairs(UnitDefs) do
     end
 end
 
-local function dump(o)
-    if type(o) == 'table' then
-       local s = '{ '
-       for k,v in pairs(o) do
-          if type(k) ~= 'number' then k = '"'..k..'"' end
-          s = s .. '['..k..'] = ' .. dump(v) .. ','
-       end
-       return s .. '} '
-    else
-       return tostring(o)
-    end
+local iconSize = 50
+
+local function applyColor(currentColor, newColor)
+    currentColor.r = newColor.r
+    currentColor.g = newColor.g
+    currentColor.b = newColor.b
+    currentColor.a = newColor.a
 end
 
 function widget:Initialize()
@@ -72,10 +66,11 @@ function widget:Initialize()
     if not MasterFramework then
         error("[WidgetName] Error: MasterFramework " .. requiredFrameworkVersion .. " not found! Removing self.")
     end
-	
-	spectatorMode = Spring.GetSpectatingState()
 
-    backgroundColor = MasterFramework:Color(0, 0, 0, 0)
+    spectatorMode = Spring.GetSpectatingState()
+    countersCache = {}
+
+    backgroundColor = MasterFramework:Color(0, 0, 0, 0.6)
     red = MasterFramework:Color(1, 0, 0, 1)
     green = MasterFramework:Color(0, 1, 0, 1)
     yellow = MasterFramework:Color(1, 1, 0, 1)
@@ -83,127 +78,153 @@ function widget:Initialize()
 
     contentStack = MasterFramework:HorizontalStack({}, MasterFramework:Dimension(8), 1)
 
-    key =
-        MasterFramework:InsertElement(
-        MasterFramework:MovableFrame(
-            "Buildings Tracker",
-            MasterFramework:PrimaryFrame(
-                MasterFramework:MarginAroundRect(
-                    contentStack,
-                    MasterFramework:Dimension(5),
-                    MasterFramework:Dimension(5),
-                    MasterFramework:Dimension(5),
-                    MasterFramework:Dimension(5),
-                    {backgroundColor},
-                    MasterFramework:Dimension(5),
-                    true
-                )
+    key = MasterFramework:InsertElement(
+            MasterFramework:MovableFrame(
+                    widgetName,
+                    MasterFramework:PrimaryFrame(
+                            MasterFramework:MarginAroundRect(
+                                    contentStack,
+                                    MasterFramework:Dimension(5),
+                                    MasterFramework:Dimension(5),
+                                    MasterFramework:Dimension(5),
+                                    MasterFramework:Dimension(5),
+                                    { backgroundColor },
+                                    MasterFramework:Dimension(5),
+                                    true
+                            )
+                    ),
+                    1700,
+                    900
             ),
-            1700,
-            900
-        ),
-        "Buildings Tracker",
-        MasterFramework.layerRequest.bottom()
+            widgetName,
+            MasterFramework.layerRequest.bottom()
     )
 end
 
-local function displayPinpointers(pinpointersCount)
-    local color
-    if pinpointersCount == 0 then
-        color = red
-    elseif pinpointersCount < 3 then
-        color = yellow
-    else
-        color = green
+local function UnitCounter(id, greenThreshold)
+    if countersCache[id] then
+        return countersCache[id]
     end
-
-    local pinpointerText = MasterFramework:Text(string.format("%d", pinpointersCount), color)
-
-    return MasterFramework:VerticalStack({
+    local currentColor = MasterFramework:Color(1, 1, 1, 1)
+    local counterText = MasterFramework:Text("", currentColor)
+    local counter = MasterFramework:VerticalStack({
         MasterFramework:Rect(
-            MasterFramework:Dimension(50),
-            MasterFramework:Dimension(50),
-            MasterFramework:Dimension(3),
-            {MasterFramework:Image("#" .. pinpointerDefIDs[1])}
+                MasterFramework:Dimension(iconSize),
+                MasterFramework:Dimension(iconSize),
+                MasterFramework:Dimension(3),
+                { MasterFramework:Image("#" .. pinpointerDefIDs[1]) }
         ),
-        pinpointerText},
-        MasterFramework:Dimension(8),
-        1
+        counterText },
+            MasterFramework:Dimension(8),
+            1
     )
+
+    function counter:Update(unitCount)
+        local newColor
+        if unitCount == 0 then
+            newColor = red
+        elseif greenThreshold and unitCount < greenThreshold then
+            newColor = yellow
+        else
+            newColor = green
+        end
+
+        counterText:SetString(string.format("%d", unitCount))
+        applyColor(currentColor, newColor)
+    end
+
+    countersCache[id] = counter
+    return counter
 end
 
-local function display(buildings, icon)
-    local stockpile = 0
-    local maxStockpilePercent = 0
-    local color
+local function UnitWithStockpileCounter(id, icon)
+    if countersCache[id] then
+        return countersCache[id]
+    end
     local unitsToSelect = {}
+    local stockpileColor = MasterFramework:Color(1, 0, 0, 1)
+    local stockpileText = MasterFramework:Text("", stockpileColor)
+    local buildPercentText = MasterFramework:Text("", white)
 
-    for _, unitId in ipairs(buildings) do
-        local unitStockpile, _, unitBuildPercent = Spring.GetUnitStockpile(unitId)
-        if unitStockpile > 0 then
-            table.insert(unitsToSelect, unitId)
-        end
-        stockpile = stockpile + unitStockpile
-        if unitBuildPercent * 100 > maxStockpilePercent then
-            maxStockpilePercent = unitBuildPercent * 100
-        end
-    end
-
-    if stockpile == 0 then
-        color = red
-    else
-        color = green
-    end
-    local stockpileText = MasterFramework:Text(string.format("%d", stockpile), color)
-    local buildPercentText = MasterFramework:Text(string.format("%02d%%", maxStockpilePercent), white)
-	
-	if #buildings == 0 then 
-		buildPercentText = nil
-		stockpileText = nil
-	end
-
-    return MasterFramework:VerticalStack({
+    local counter = MasterFramework:VerticalStack({
         MasterFramework:Button(
-            MasterFramework:Rect(
-                MasterFramework:Dimension(50),
-                MasterFramework:Dimension(50),
-                MasterFramework:Dimension(3),
-                {MasterFramework:Image("#" .. icon)}
-            ),
-            function()
-                local _, _, _, shift = Spring.GetModKeyState()
+                MasterFramework:Rect(
+                        MasterFramework:Dimension(iconSize),
+                        MasterFramework:Dimension(iconSize),
+                        MasterFramework:Dimension(3),
+                        { MasterFramework:Image("#" .. icon) }
+                ),
+                function()
+                    local _, _, _, shift = Spring.GetModKeyState()
 
-                if not shift then
-                    unitsToSelect = {unitsToSelect[1]}
+                    if not shift then
+                        unitsToSelect = { unitsToSelect[1] }
+                    end
+                    Spring.SelectUnitArray(unitsToSelect, shift)
                 end
-                Spring.SelectUnitArray(unitsToSelect, shift)
-            end
         ),
         MasterFramework:HorizontalStack({
+            buildPercentText,
             stockpileText,
-            buildPercentText
         },
-        MasterFramework:Dimension(6),
-        1
+                MasterFramework:Dimension(6),
+                1
         )
     },
-    MasterFramework:Dimension(8),
-    1
+            MasterFramework:Dimension(8),
+            1
     )
+
+    function counter:Update(units)
+        local stockpile = 0
+        local maxStockpilePercent = 0
+        local color
+        unitsToSelect = {}
+
+        if #units == 0 then
+            stockpileText:SetString("")
+            buildPercentText:SetString("")
+            return
+        end
+
+        for _, unitId in ipairs(units) do
+            local unitStockpile, _, unitBuildPercent = Spring.GetUnitStockpile(unitId)
+            if unitStockpile > 0 then
+                table.insert(unitsToSelect, unitId)
+            end
+            stockpile = stockpile + unitStockpile
+            if unitBuildPercent * 100 > maxStockpilePercent then
+                maxStockpilePercent = unitBuildPercent * 100
+            end
+        end
+
+        if stockpile == 0 then
+            color = red
+        else
+            color = green
+        end
+
+        stockpileText:SetString(string.format("%d", stockpile))
+        applyColor(stockpileColor, color)
+        buildPercentText:SetString(string.format("%2d%%", maxStockpilePercent))
+    end
+
+    countersCache[id] = counter
+    return counter
 end
 
 local function findUnits(teamIDs, unitDefIDs)
-	return table.reduce(teamIDs, function(acc, teamID)
-		table.append(acc, Spring.GetTeamUnitsByDefs(teamID, unitDefIDs))
-		return acc
-	end, {})
+    return table.reduce(teamIDs, function(acc, teamID)
+        table.append(acc, Spring.GetTeamUnitsByDefs(teamID, unitDefIDs))
+        return acc
+    end, {})
 end
 
 function widget:GameFrame(n)
     contentStack.members = {}
     local pinpointersCount = 0
     local allUnits = Spring.GetAllUnits()
-	
+
     for _, unitId in ipairs(allUnits) do
         if Spring.IsUnitAllied(unitId) then
             if pinpointerNames[UnitDefs[Spring.GetUnitDefID(unitId)].name] then
@@ -211,34 +232,30 @@ function widget:GameFrame(n)
             end
         end
     end
-    table.insert(contentStack.members, displayPinpointers(pinpointersCount))
-	
-	local teamId
-	if spectatorMode then
-		teamId = Spring.GetMyAllyTeamID()
-	else
-		teamId = Spring.GetMyTeamID()
-	end
-	
-    local nukes
-    local junos
-	if spectatorMode then
-		local teamIds = Spring.GetTeamList(teamId)
-		nukes = findUnits(teamIds, nukeDefIDs)
-		junos = findUnits(teamIds, junoDefIDs)
-	else
-		nukes = Spring.GetTeamUnitsByDefs(teamId, nukeDefIDs)
-		junos = Spring.GetTeamUnitsByDefs(teamId, junoDefIDs)
-	end
-	
-    if (#junos > 0 or spectatorMode) then
-        table.insert(contentStack.members, display(junos, junoDefIDs[1]))
-    end
-    if (#nukes > 0 or spectatorMode) then
-        table.insert(contentStack.members, display(nukes, nukeDefIDs[1]))
+    table.insert(contentStack.members, UnitCounter(pinpointersId, 3))
+
+    local teamId, teamIds
+    if spectatorMode then
+        teamId = Spring.GetMyAllyTeamID()
+        teamIds = Spring.GetTeamList(teamId)
+    else
+        teamId = Spring.GetMyTeamID()
+        teamIds = { teamId }
     end
 
-    backgroundColor.a = 0.6
+    local nukes = findUnits(teamIds, nukeDefIDs)
+    local junos = findUnits(teamIds, junoDefIDs)
+
+    if (#junos > 0 or spectatorMode) then
+        table.insert(contentStack.members, UnitWithStockpileCounter("junos", junoDefIDs[1]))
+    end
+    if (#nukes > 0 or spectatorMode) then
+        table.insert(contentStack.members, UnitWithStockpileCounter("nukes", nukeDefIDs[1]))
+    end
+
+    countersCache[pinpointersId]:Update(pinpointersCount)
+    countersCache[nukesId]:Update(nukes)
+    countersCache[junosId]:Update(junos)
 end
 
 function widget:Shutdown()
