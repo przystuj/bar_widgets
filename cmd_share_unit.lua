a4601ffc118e53831a7c568c81abb160a0a27d2c
SuperKitowiec <superkitowiec@gmail.com>
2024-11-11 18:12:42 +0100

    cmd_share_unit.lua v3
    - add aoe indicator to the command
    - removed quick_share_near_cursor action

--- cmd_share_unit.lua
+++ cmd_share_unit.lua
@@ -2,45 +2,67 @@ function widget:GetInfo()
     return {
         name = "Share Unit Command",
         desc = "Adds a command which allows you to quickly share unit to other player. Just target the command on any allied unit and you will share to this player",
-        author = "Stolen by SuperKitowiec from citrine",
+        author = "SuperKitowiec",
         date = "2024",
         license = "GNU GPL, v2 or later",
-        version = 2,
+        version = 3,
         layer = 0,
         enabled = true,
         handler = true,
     }
 end
 
--- engine call optimizations
--- =========================
-local SpringGetUnitsInCylinder = Spring.GetUnitsInCylinder
-local SpringGetMouseState = Spring.GetMouseState
-local SpringGetMyTeamID = Spring.GetMyTeamID
-local SpringTraceScreenRay = Spring.TraceScreenRay
-local SpringGetUnitTeam = Spring.GetUnitTeam
-local SpringGetSelectedUnits = Spring.GetSelectedUnits
-local SpringGetTeamAllyTeamID = Spring.GetTeamAllyTeamID
-local SpringShareResources = Spring.ShareResources
-local SpringI18N = Spring.I18N
-local SpringGetSpectatingState = Spring.GetSpectatingState
-local SpringWorldToScreenCoords = Spring.WorldToScreenCoords
-local SpringPlaySoundFile = Spring.PlaySoundFile
+--------------------------------------------------------------------------------
+--vars
+--------------------------------------------------------------------------------
+local aoeColor = { 0.7, 0.7, 0.7, 1 }
+local aoeLineWidthMult = 100
+local circleDivs = 96
+local numAoECircles = 5
+local circleList
+local secondPart = 0
+local mouseDistance = 1000
+local range = 200
 
-local function dump(o)
-    if type(o) == 'table' then
-        local s = '{ '
-        for k, v in pairs(o) do
-            if type(k) ~= 'number' then
-                k = '"' .. k .. '"'
-            end
-            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
-        end
-        return s .. '} '
-    else
-        return tostring(o)
-    end
-end
+--------------------------------------------------------------------------------
+--speedups
+--------------------------------------------------------------------------------
+local GetUnitsInCylinder = Spring.GetUnitsInCylinder
+local GetMyTeamID = Spring.GetMyTeamID
+local GetUnitTeam = Spring.GetUnitTeam
+local GetSelectedUnits = Spring.GetSelectedUnits
+local GetTeamAllyTeamID = Spring.GetTeamAllyTeamID
+local ShareResources = Spring.ShareResources
+local I18N = Spring.I18N
+local GetSpectatingState = Spring.GetSpectatingState
+local WorldToScreenCoords = Spring.WorldToScreenCoords
+local PlaySoundFile = Spring.PlaySoundFile
+local GetTeamColor = Spring.GetTeamColor
+local GetActiveCommand = Spring.GetActiveCommand
+local GetCameraPosition = Spring.GetCameraPosition
+local GetMouseState = Spring.GetMouseState
+local GetUnitPosition = Spring.GetUnitPosition
+local TraceScreenRay = Spring.TraceScreenRay
+
+local glBeginEnd = gl.BeginEnd
+local glCallList = gl.CallList
+local glCreateList = gl.CreateList
+local glColor = gl.Color
+local glDeleteList = gl.DeleteList
+local glLineWidth = gl.LineWidth
+local glPopMatrix = gl.PopMatrix
+local glPushMatrix = gl.PushMatrix
+local glScale = gl.Scale
+local glTranslate = gl.Translate
+local glVertex = gl.Vertex
+local GL_LINE_LOOP = GL.LINE_LOOP
+
+local PI = math.pi
+local cos = math.cos
+local sin = math.sin
+local floor = math.floor
+local sqrt = math.sqrt
+local max = math.max
 
 local function tablelength(T)
     local count = 0
@@ -59,12 +81,82 @@ local CMD_SHARE_UNIT_TO_TARGET_DEFINITION = {
     action = 'quick_share_to_target',
 }
 
-local myTeamID = SpringGetMyTeamID()
-local myAllyTeamID = SpringGetTeamAllyTeamID(myTeamID)
+local myTeamID = GetMyTeamID()
+local myAllyTeamID = GetTeamAllyTeamID(myTeamID)
+
+local function GetSecondPart(offset)
+    local result = secondPart + (offset or 0)
+    return result - floor(result)
+end
+
+local function UnitCircleVertices()
+    for i = 1, circleDivs do
+        local theta = 2 * PI * i / circleDivs
+        glVertex(cos(theta), 0, sin(theta))
+    end
+end
+
+local function DrawUnitCircle()
+    glBeginEnd(GL_LINE_LOOP, UnitCircleVertices)
+end
+
+local function SetupDisplayLists()
+    circleList = glCreateList(DrawUnitCircle)
+end
+
+local function DeleteDisplayLists()
+    glDeleteList(circleList)
+end
+
+local function DrawCircle(x, y, z, radius)
+    glPushMatrix()
+    glTranslate(x, y, z)
+    glScale(radius, radius, radius)
+
+    glCallList(circleList)
+
+    glPopMatrix()
+end
+
+local function DrawAoE(tx, ty, tz, color)
+    glLineWidth(max(aoeLineWidthMult * range / mouseDistance, 0.5))
+
+    for i = 1, numAoECircles do
+        local proportion = i / (numAoECircles + 1)
+        local radius = range * proportion
+        local alpha = color[4] * (1 - proportion) / (1 - proportion) * (1 - GetSecondPart(0))
+        glColor(color[1], color[2], color[3], alpha)
+        DrawCircle(tx, ty, tz, radius)
+    end
+
+    glColor(1, 1, 1, 1)
+    glLineWidth(1)
+end
+
+local function GetMouseTargetPosition()
+    local mx, my = GetMouseState()
+    local mouseTargetType, mouseTarget = TraceScreenRay(mx, my)
+    if mouseTarget and mouseTargetType then
+        if mouseTargetType == "ground" then
+            return mouseTarget[1], mouseTarget[2], mouseTarget[3]
+        elseif mouseTargetType == "unit" then
+            return GetUnitPosition(mouseTarget)
+        elseif mouseTargetType == "feature" then
+            local _, mouseTarget = TraceScreenRay(mx, my, true)
+            if mouseTarget then
+                return mouseTarget[1], mouseTarget[2], mouseTarget[3]
+            end
+        else
+            return nil
+        end
+    else
+        return nil
+    end
+end
 
 local function FindTeam(mx, my)
-    local _, cUnitID = SpringTraceScreenRay(mx, my, true)
-    local foundUnits = SpringGetUnitsInCylinder(cUnitID[1], cUnitID[3], 200)
+    local _, cUnitID = TraceScreenRay(mx, my, true)
+    local foundUnits = GetUnitsInCylinder(cUnitID[1], cUnitID[3], range)
 
     if #foundUnits < 1 then
         return nil
@@ -73,8 +165,8 @@ local function FindTeam(mx, my)
     local unitTeamCounters = {}
 
     for _, unitId in ipairs(foundUnits) do
-        local unitTeamId = SpringGetUnitTeam(unitId)
-        if unitTeamId ~= myTeamID and SpringGetTeamAllyTeamID(unitTeamId) == myAllyTeamID then
+        local unitTeamId = GetUnitTeam(unitId)
+        if unitTeamId ~= myTeamID and GetTeamAllyTeamID(unitTeamId) == myAllyTeamID then
             unitTeamId = tostring(unitTeamId)
             if unitTeamCounters[unitTeamId] == nil then
                 unitTeamCounters[unitTeamId] = 1
@@ -99,54 +191,75 @@ local function FindTeam(mx, my)
     return selectedTeam
 end
 
-function widget:CommandNotify(cmdID, cmdParams, cmdOpts)
+local function GetMouseDistance()
+    local cx, cy, cz = GetCameraPosition()
+    local mx, my, mz = GetMouseTargetPosition()
+    if not mx then
+        return nil
+    end
+    local dx = cx - mx
+    local dy = cy - my
+    local dz = cz - mz
+    return sqrt(dx * dx + dy * dy + dz * dz)
+end
+
+function widget:DrawWorld()
+
+    local _, cmd, _ = GetActiveCommand()
+
+    if cmd ~= CMD_SHARE_UNIT_TO_TARGET then
+        return
+    end
+
+    local tx, ty, tz = GetMouseTargetPosition()
+    if (not tx) then
+        return
+    end
+
+    mouseDistance = GetMouseDistance() or 1000
+
+    local mouseX, mouseY = WorldToScreenCoords(tx, ty, tz)
+    local selectedTeam = FindTeam(mouseX, mouseY)
+    local selectedColor = aoeColor
+    if selectedTeam then
+        local tred, tgreen, tblue = GetTeamColor(selectedTeam)
+        selectedColor = { tred, tgreen, tblue, 1 }
+    end
+    DrawAoE(tx, ty, tz, selectedColor)
+end
+
+function widget:CommandNotify(cmdID, cmdParams, _)
     if cmdID == CMD_SHARE_UNIT_TO_TARGET then
-        local targetTeamID = nil
-        if #cmdParams ~= 1 and #cmdParams ~= 3  then
+        local targetTeamID
+        if #cmdParams ~= 1 and #cmdParams ~= 3 then
             return true
         elseif #cmdParams == 1 then
             -- click on unit
             local targetUnitID = cmdParams[1]
-            targetTeamID = SpringGetUnitTeam(targetUnitID)
+            targetTeamID = GetUnitTeam(targetUnitID)
         elseif #cmdParams == 3 then
             -- click on the ground
-            local mouseX, mouseY = SpringWorldToScreenCoords(cmdParams[1], cmdParams[2], cmdParams[3])
+            local mouseX, mouseY = WorldToScreenCoords(cmdParams[1], cmdParams[2], cmdParams[3])
             targetTeamID = FindTeam(mouseX, mouseY)
         end
 
-        if targetTeamID == nil or targetTeamID == myTeamID or SpringGetTeamAllyTeamID(targetTeamID) ~= myAllyTeamID then
+        if targetTeamID == nil or targetTeamID == myTeamID or GetTeamAllyTeamID(targetTeamID) ~= myAllyTeamID then
             -- invalid target, don't do anything
             return true
         end
 
-        SpringShareResources(targetTeamID, "units")
-        SpringPlaySoundFile("beep4", 1, 'ui')
+        ShareResources(targetTeamID, "units")
+        PlaySoundFile("beep4", 1, 'ui')
         return false
     end
 end
 
-local function QuickShareUnit()
-    local selectedUnits = SpringGetSelectedUnits()
-    if #selectedUnits < 1 then
-        return
-    end
-
-    local mx, my, _, mmb, _, mouseOffScreen, cameraPanMode = SpringGetMouseState()
-    if mouseOffScreen or mmb or cameraPanMode then
-        return
-    end
-
-    local selectedTeam = FindTeam(mx, my)
-    SpringShareResources(selectedTeam, "units")
-    SpringPlaySoundFile("beep4", 1, 'ui')
-end
-
 function widget:CommandsChanged()
-    if SpringGetSpectatingState() then
+    if GetSpectatingState() then
         return
     end
 
-    local selectedUnits = SpringGetSelectedUnits()
+    local selectedUnits = GetSelectedUnits()
     if #selectedUnits > 0 then
         local customCommands = widgetHandler.customCommands
         customCommands[#customCommands + 1] = CMD_SHARE_UNIT_TO_TARGET_DEFINITION
@@ -154,11 +267,15 @@ function widget:CommandsChanged()
 end
 
 function widget:Initialize()
-    widgetHandler.actionHandler:AddAction(self, "quick_share_near_cursor", QuickShareUnit, nil, 'p')
-    SpringI18N.load({
+    SetupDisplayLists()
+    I18N.load({
         en = {
             ["ui.orderMenu.quick_share_to_target"] = "Share Unit",
             ["ui.orderMenu.quick_share_to_target_tooltip"] = "Share unit to target player.",
         }
     })
+end
+
+function widget:Shutdown()
+    DeleteDisplayLists()
 end
